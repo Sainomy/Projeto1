caef06f764cd1d4a3dc901631284b5fc5aedcb87
Sainomy <simonycogoy@gmail.com>
2021-08-23 15:16:43 -0300

    5commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 var usuarioRoute = require("./routes/usuarioRoute");
+var path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/usuario", usuarioRoute);
 
